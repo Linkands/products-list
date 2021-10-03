f2687ba648a8d6fba2f15be7ef8c61ba1148ab3a
Andres Link <andres.link@mail.ru>
2021-10-03 11:03:00 +0300

    added form component and input values states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import React from 'react'
+import Form from './components/Form'
+
 function App() {
-  return <div className="App"></div>
+  const [name, setName] = React.useState('')
+  const [image, setImage] = React.useState('')
+  const [price, setPrice] = React.useState('')
+  const [products, setProducts] = React.useState([])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    switch (name) {
+      case 'name':
+        setName(value)
+        break
+      case 'image':
+        setImage(value)
+        break
+      case 'price':
+        setPrice(value)
+        break
+    }
+  }
+
+  return (
+    <>
+      <Form
+        name={name}
+        image={image}
+        price={price}
+        handleChange={handleChange}
+      ></Form>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Form/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Form.module.css'
+
+function Form({ name, price, image, handleChange }) {
+  return (
+    <div className={styles.wrapper}>
+      <h2>Form</h2>
+      <form>
+        <input
+          name="image"
+          value={image}
+          required
+          placeholder="Product image(URL)"
+          onChange={handleChange}
+        ></input>
+        <input
+          name="name"
+          value={name}
+          required
+          placeholder="Product name"
+          onChange={handleChange}
+        ></input>
+        <input
+          name="price"
+          value={price}
+          required
+          placeholder="Product price"
+          onChange={handleChange}
+        ></input>
+        <button type="submit">Add product</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form
